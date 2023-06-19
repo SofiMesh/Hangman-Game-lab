381114773bd6a1ad9e034fedde92352c76923abe
SofiMesh <sofimeshvildishvili@gmail.com>
2023-06-19 13:51:26 -0400

    CSS part

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+//display words for the game
+
+const wordOptions = ["Lion", "Tiger", "Wolf", "Fox", "Racoon", "Monkey"];
+
+//play game button
+
+
+
+
+//display input box
+
+
+//display letter container
+
+
+//display hanging device
+
+//generate words
+
+//count points
+
+//hide letters and clear the previous word
+
+//disable  buttons
+
+
