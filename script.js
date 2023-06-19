6c73f58e0d0e4e474505a1091435e8fe0c312563
SofiMesh <sofimeshvildishvili@gmail.com>
2023-06-19 19:01:51 -0400

    part of JS done

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
 
+//references
 
+const letterContainer = document.getElementById("letter-container");
+const inputBox = document.getElementById("input-box");
+const result = document.getElementById("result");
+// const canvas = document.getElementById("canvas");
+const newGame = document.getElementById("new-game");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
 
 
 
 //display words for the game
 
-const wordOptions = ["Lion", "Tiger", "Wolf", "Fox", "Racoon", "Monkey"];
+let wordOptions = ["Lion", "Tiger", "Wolf", "Fox", "Racoon", "Monkey"];
+
+//count points
+let winCount = 0;
+let count = 0;
+let chosenWord = "";
+
+
+//
+
+let button = document.createElement("div");
+for (let value in options) {
+    button.unnerHTML += `button class="options" onclick="generateWord(`${value}`)">${value}</button`;
+}
+
+
+//letters
+const blocker = () => {
+    let letterButtons = document.querySelectorAll(".letters");
+
+letterButtons.forEach((button) => {
+    button.disable.true;
+});
+newGame.classList.remove("hide");
+}
+
+
 
 //play game button
 
@@ -15,14 +49,10 @@ const wordOptions = ["Lion", "Tiger", "Wolf", "Fox", "Racoon", "Monkey"];
 //display input box
 
 
-//display letter container
-
-
 //display hanging device
 
 //generate words
 
-//count points
 
 //hide letters and clear the previous word
 
